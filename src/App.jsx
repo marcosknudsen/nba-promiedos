2baf37a1bd8c9c31c4fda4370fd85c1d3b9683f5
Marcos Knudsen <marcosknu14@gmail.com>
2024-01-02 13:46:43 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import "./App.css"
 import Today from "./Today.jsx";
 import Tomorrow from "./Tomorrow.jsx";
 import Yesterday from "./Yesterday.jsx";
@@ -11,6 +12,9 @@ function App() {
         <Route path="/nba-promiedos/tomorrow" element={<Tomorrow/>} />
         <Route path="/nba-promiedos/yesterday" element={<Yesterday/>} />
       </Routes>
+      <footer>
+        Este sitio web es un proyecto personal inspirado en el mítico <a href="https://www.promiedos.com.ar/">Promiedos</a> con la única intencion de aprender a usar las tecnologias implicadas.
+      </footer>
     </>
   );
 }
